06bd10584f16eba65da4cd36b46877e2e3aac3b5
Welliton Scheer <wellitonscheer@gmail.com>
2021-05-18 16:57:05 -0300

    Buscar CEP

---
+++ Test/Pages/CEP.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Net;
+using System.Threading.Tasks;
+namespace Test.Models
+{
+    public class CEP
+    {
+        public string BuscarCidade(Cidade cidade)
+        {
+            string strurlTest = string.Format("https://viacep.com.br/ws/{0}/json/",cidade.CEP);
+            WebRequest requestObjGet = WebRequest.Create(strurlTest);
+            requestObjGet.Method = "GET";
+            HttpWebResponse responseObjGet = null;
+            responseObjGet = (HttpWebResponse)requestObjGet.GetResponse();
+            string streesulttest = null;
+            using (Stream stream = responseObjGet.GetResponseStream())
+            {
+                StreamReader sr = new StreamReader(stream);
+                streesulttest = sr.ReadToEnd();
+                sr.Close();
+            }
+            return streesulttest;
+            //WebRequest requestObjPost = WebRequest.Create(strurlTest);
+            //requestObjPost.Method = "POST";
+            //requestObjPost.ContentType = "application/json";
+            //string postData = "{\"title\":\"testdata\",\"body\":\"testbody\",\"userId\":\"50\"}";
+            //using (var streamWriter = new StreamWriter(requestObjPost.GetRequestStream()))
+            //{
+            //    streamWriter.Write(postData);
+            //    streamWriter.Flush();
+            //    streamWriter.Close();
+            //    var httpResponse = (HttpWebResponse)requestObjPost.GetResponse();
+            //    using (var streamReader = new StreamReader(httpResponse.GetResponseStream()))
+            //    {
+            //        var result2 = streamReader.ReadToEnd();
+            //    }
+            //}
+            //return "burro";
+        }
+    }
+}
---
+++ Test/Pages/Indexcshtml.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.Extensions.Logging;
+using Test.Models;
+
+namespace Test.Pages
+{
+    public class IndexModel : PageModel
+    {
+        
+        public string Name { get; set; }
+
+        private readonly ILogger<IndexModel> _logger;
+
+        public IndexModel(ILogger<IndexModel> logger)
+        {
+            _logger = logger;
+        }
+
+        public void OnGet()
+        {
+            Name = "Test do Name";
+        }
+        public void OnPostSubmit(Cidade cidade)
+        {
+            var cep = new CEP();
+            var nomeCidade = cep.BuscarCidade(cidade);
+            this.Name = nomeCidade;
+        }
+    }
+}
